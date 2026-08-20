b0a1a44502cee249e19af2910f9bc15aa0d6e702
Live Productions <liveproductionsgt@gmail.com>
2026-08-20 15:20:53 -0600

    Ampliar panel de total de Luxury Travel

--- luxury/public/sw.js
+++ luxury/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "luxury-travel-v70";
+const CACHE_NAME = "luxury-travel-v71";
 const SCOPE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, "");
 const scopedPath = (pathname = "/") =>
   `${SCOPE_PATH}${pathname.startsWith("/") ? pathname : `/${pathname}`}`;
